818784d5ac3de85dc6b6fcc3a058026c96077332
Tyson Smith <tyson.w.smith@gmail.com>
2025-07-18 09:36:13 -0700

    feat: PageExplorer().get() now returns a PageLoad value

--- src/page_explorer/__init__.py
+++ src/page_explorer/__init__.py
@@ -3,6 +3,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """page explorer module"""
 
-from .page_explorer import ExplorerError, PageExplorer
+from .page_explorer import ExplorerError, PageExplorer, PageLoad
 
-__all__ = ("ExplorerError", "PageExplorer")
+__all__ = ("ExplorerError", "PageExplorer", "PageLoad")
